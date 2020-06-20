89ae42801e344e0c3e8b01c980d8576e41cefa46
vlary <vlary@PC.lan>
2020-06-20 19:01:52 +0800

    Site updated: 2020-06-20 19:01:51

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Sat Jun 20 2020 19:01:49 GMT+0800 (中国标准时间)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
